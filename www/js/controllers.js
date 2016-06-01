9d4f2ef55982afeb7fd7a53f376ef7ff50b77936
Pablo Reyes <pablo.reyesd@usach.cl>
2016-06-01 17:29:55 -0400

    solicitud casi lista

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,37 +1,7 @@
 angular.module('starter')
 
 .controller('AppCtrl', function($scope, $state, $ionicPopup, $ionicModal, AuthService) {
-  $ionicModal.fromTemplateUrl('templates/modal.html', {
-      id: '1', // We need to use and ID to identify the modal that is firing the event!
-      scope: $scope,
-      backdropClickToClose: false,
-      animation: 'slide-in-up'
-    }).then(function(modal) {
-      $scope.oModal1 = modal;
-    });
-    $ionicModal.fromTemplateUrl('templates/modal2_confirmacion.html', {
-      id: '2', // We need to use and ID to identify the modal that is firing the event!
-      scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-  
-  $scope.openModal = function(index) {
-      if (index == 1) $scope.oModal1.show();
-      else $scope.oModal2.show();
-    };
-
-    $scope.closeModal = function(index) {
-      if (index == 1) $scope.oModal1.hide();
-      else $scope.oModal2.hide();
-    };
-    $scope.$on('$destroy', function() {
-      console.log('Destroying modals...');
-      $scope.oModal1.remove();
-      $scope.oModal2.remove();
-    });
-
-$scope.showAlert = function() {
+  $scope.showAlert = function() {
    var alertPopup = $ionicPopup.alert({
      title: 'Solicitud exitosa!',
      template: 'Enseguida recibirás un email con los detalles de la solicitud realizada.'
@@ -89,17 +59,71 @@ $scope.showAlert = function() {
   });
 })
 
-.controller('solicitudCtrl',function(getData,AuthService){
-  getData.getDocuments(AuthService.token(),function(response){
-      /*document.getElementById('solicitudes').innerHTML += '<li class="item item-checkbox checkbox-circle">';
-      document.getElementById('solicitudes').innerHTML += '\t<label class="checkbox">';
-      document.getElementById('solicitudes').innerHTML += '\t\t<input type="checkbox">';
-      document.getElementById('solicitudes').innerHTML += '\t</label>';
-      document.getElementById('solicitudes').innerHTML += '\t<h2>Nombre Documento</h2>';
-      document.getElementById('solicitudes').innerHTML += '\t<p>valor: $5.000.000</p>';
-      document.getElementById('solicitudes').innerHTML += '</li>';*/
+.controller('solicitudCtrl',function(getData,AuthService, $ionicModal, $scope, $ionicLoading){
+  var solicitudes = '';
+  $ionicModal.fromTemplateUrl('templates/modal.html', {
+      id: '1', // We need to use and ID to identify the modal that is firing the event!
+      scope: $scope,
+      backdropClickToClose: false,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.oModal1 = modal;
     });
+   $ionicModal.fromTemplateUrl('templates/modal2_confirmacion.html', {
+      id: '2', // We need to use and ID to identify the modal that is firing the event!
+      scope: $scope
+    }).then(function(modal) {
+    $scope.modal = modal;
+    });
+  
+    openModal = function(index) {
+      if (index == 1) $scope.oModal1.show();
+      else $scope.oModal2.show();
+    };
+
+    closeModal = function(index) {
+      if (index == 1) $scope.oModal1.hide();
+      else $scope.oModal2.hide();
+    };
+    $scope.$on('$destroy', function() {
+      console.log('Destroying modals...');
+      $scope.oModal1.remove();
+      $scope.oModal2.remove();
+    });
+
+  getData.getDocuments(AuthService.token(),function(response){
+    solicitudes = response;
+    var Format = wNumb({
+         prefix: '$',
+        decimals: 0,
+        thousand: '.'
+      });
+      var botoncito = '';
+      var i = 0;
+      var aux = undefined;
+      while(response[i]){
+        aux = response[i];
+        botoncito += '<li class="item item-checkbox checkbox-circle">';
+        botoncito += '\t<label class="checkbox">';
+        botoncito += '\t\t<input name = "'+i+'" type="checkbox">';
+        botoncito += '\t</label>';
+        botoncito += '\t<h3><a ng-click="openModal(1)">'+aux["socialReasonReceiver"]+'</a></h3>';
+        botoncito += '\t<p>Monto: '+Format.to(aux["ammount"])+'</p>';
+        botoncito += '\t<p>Folio: '+aux["folio"]+'</p>';
+        botoncito += '</li>';
+        i = i+1;
+      }
+      document.getElementById('solicitudes').innerHTML += botoncito ;
+    });
+  $scope.solicitar= function(){
+    var selected = [];
+    $('#solicitudes input:checked').each(function() {
+      selected.push($(this).attr('name'));
+    });
+    console.log(selected);
+  }
 })
+
 /* esto sirve para el pdf, hay que afinarlo
     $.ajax(settings).done(function (response) {
         var file = new Blob([response], {type: 'application/pdf'});
