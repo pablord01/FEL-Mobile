6f7d6420f5c6891f9df0daa983fe9e71d38fbbad
Pablo Reyes <pablo.reyesd@usach.cl>
2016-06-30 09:30:33 -0400

    commit final

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -146,7 +146,7 @@ angular.module('starter')
     }   
     else{
       var alertPopup = $ionicPopup.alert({
-        title: '¡Error!',
+        title: '¡Petición incorrecta!',
         template: 'Selecciona al menos un documento'
       });
     }
@@ -163,10 +163,46 @@ angular.module('starter')
         });
         $state.go('tabs.solicitud', {}, {reload: true});
       })
-    }
+    };
+
+
+    $(":checkbox").on("change", function() {
+  //change input #grandtotal value according check/uncheck checkboxes
+  $("#grandtotal").val(function() {
+    //declare a variable to keep the sum of the values
+    var sum = 0;
+    //using an iterator find and sum the values of checked checkboxes
+    $(":checkbox:checked").each(function() {
+      sum += ~~$(this).val();
+    });
+    $rootScope.$broadcast('suma:updated',sum);
+    $scope.suma = sum;
+  });
+});  
+
+
+//here change the value according on checked checkboxes on DOM ready event
+$("#grandtotal").val(function() {
+  var sum = 0;
+  $(":checkbox:checked").each(function() {
+    sum += ~~$(this).val();
+  });
+});
 })
 
 .controller('ofertasCtrl',function(passingData, getData,AuthService,$state, $rootScope, $ionicModal, $ionicPopup, $scope, $ionicLoading){
+  $scope.pesos = function(monto){
+    var Format = wNumb({
+      prefix: '$',
+      decimals: 0,
+      thousand: '.'
+    });
+    return Format.to(monto);
+  };
+  $scope.fecha = function(date){
+    return new Date(date).toUTCString()
+  };
+
   $scope.offers = '';
   $scope.offerspending = '';
   $scope.$on('offers:updated', function(event,response,response2) {
@@ -174,6 +210,8 @@ angular.module('starter')
     $scope.offerspending = response2;
     $state.go($state.current, {}, {reload: true});
   });
+
+
   // este trae las que tienen ofertas
   getData.getOffers(AuthService.token(),function(response){
     // este trae las que no tienen ofertas
@@ -186,10 +224,65 @@ angular.module('starter')
     $rootScope.$broadcast('showoff:updated',$scope.offers[index].offers);
     passingData.setData($scope.offers[index].offers);
     $state.go('tabs.revisar', {}, {reload: true});
+  };
+
+    var solicitudes = '';
+  $ionicModal.fromTemplateUrl('templates/modalofertas.html',function($ionicModal) {
+    $scope.modal = $ionicModal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  $scope.openModal = function(doc2) {
+    var Format = wNumb({
+      prefix: '$',
+      decimals: 0,
+      thousand: '.'
+    });
+    //MODIFICAR
+    $scope.documento = doc2[0].socialReasonReceiver;
+    $scope.folio = doc2[0].folio;
+    $scope.monto = doc2[0].ammount;
+
+    $scope.documento1 = doc2[1].socialReasonReceiver;
+    $scope.folio1 = doc2[1].folio;
+    $scope.monto1 = doc2[1].ammount;
+
+    $scope.documento2 = doc2[2].socialReasonReceiver;
+    $scope.folio2 = doc2[2].folio;
+    $scope.monto2 = doc2[2].ammount;
+
+    $scope.emision= doc2[0].emissionDate;
+    $scope.rutemisor= doc2[0].rutIssuing;
+
+ 
+
+
+
+    
+    
+  
+    $scope.modal.show();
+  }
+  $scope.closeModal = function(id) {
+    $scope.selectedId = id;
+    $scope.modal.hide();
   }
 })
 
 .controller('pendientesCtrl',function(getData,AuthService,$state, $rootScope, $ionicModal, $ionicPopup, $scope, $ionicLoading){
+  $scope.pesos = function(monto){
+    var Format = wNumb({
+      prefix: '$',
+      decimals: 0,
+      thousand: '.'
+    });
+    return Format.to(monto);
+  };
+  $scope.fecha = function(date){
+    return new Date(date).toUTCString()
+  };
   $scope.pendings = '';
   $scope.$on('pending:updated', function(event,response) {
     $scope.pendings  = response;
@@ -198,6 +291,51 @@ angular.module('starter')
   getData.getPendingSignature(AuthService.token(),function(response){
     $rootScope.$broadcast('pending:updated',response);
   });
+  var solicitudes = '';
+  $ionicModal.fromTemplateUrl('templates/modalpendiente.html',function($ionicModal) {
+    $scope.modal = $ionicModal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  $scope.openModal = function(doc, doc2) {
+    var Format = wNumb({
+      prefix: '$',
+      decimals: 0,
+      thousand: '.'
+    });
+    //MODIFICAR
+    $scope.selectedNombre = doc[0].id;
+    $scope.monto = doc[0].ammountSend;
+    $scope.expira = doc[0].expire;
+    $scope.nombrefirmante = doc[0].userName;
+    $scope.correofirmante = doc[0].userEmail;
+    $scope.factoring = doc[0].factoringName;
+    $scope.factoring = doc[0].factoringName;
+    $scope.documento = doc2[0].socialReasonReceiver;
+    $scope.idDoc = doc2[0].idDocument;
+    $scope.montodoc = doc2[0].ammount;
+    $scope.erp = doc2[0].erpType;
+    $scope.emision= doc2[0].emissionDate;
+    $scope.fechaexpiracion= doc2[0].expirationDate;
+    $scope.rutreceptor= doc2[0].receiverRUT;
+    $scope.rutemisor= doc2[0].rutIssuing;
+
+    
+
+
+
+    
+    
+  
+    $scope.modal.show();
+  }
+  $scope.closeModal = function(id) {
+    $scope.selectedId = id;
+    $scope.modal.hide();
+  }
+
 })
 
 .controller('showOffers',function($ionicHistory,  passingData, getData,AuthService,$state, $rootScope, $ionicModal, $ionicPopup, $scope, $ionicLoading){
@@ -216,6 +354,8 @@ angular.module('starter')
     passingData.setData({});
     $state.go('tabs.ofertas', {}, {reload: true});
   };
+
+
 })
 
 .controller('offerDetails',function($ionicHistory,  passingData, getData,AuthService,$state, $rootScope, $ionicModal, $ionicPopup, $scope, $ionicLoading){
